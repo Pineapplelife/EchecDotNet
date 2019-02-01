89615415c5f9a7bca88dab2a3612ba86d64d38b7
Unknown <mgueribout@gmail.com>
2019-02-01 03:31:45 +0100

    IsArriveeOnEnnemy()
    Fou / Roi / Tour

--- EchecLibrary/Piece/Fou.cs
+++ EchecLibrary/Piece/Fou.cs
@@ -26,82 +26,84 @@ namespace Fr.Coding.ChessMate.Models
         }
         public override bool IsTrajectoireLibre(int xStart, int yStart, int xEnd, int yEnd, Piece[,] echiquier)
         {
-            if (0 < xEnd && xEnd < 7 && 0 < yEnd && yEnd < 7)
-            {
+            if (0 < xEnd && xEnd < 7 && 0 < yEnd && yEnd < 7) {
                 //Diagonale BAS Droit
-                if (xStart < xEnd && yStart < yEnd)
-                {
-                    while (xStart < xEnd && yStart < yEnd)
-                    {
-                        if (echiquier[xStart, yStart] == null)
-                        {
-                            xStart++;
-                            yStart++;
-                            continue;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    }
+                if (xStart < xEnd && yStart < yEnd) {
+                    while (xStart < xEnd && yStart < yEnd) {
+                        if (echiquier[xStart, yStart] == null) {
+                            xStart++;
+                            yStart++;
+                            continue;
+                        }
+                        else {
+                            if (echiquier[xEnd, yEnd] != null) {
+                                IsArriveeOnEnnemy(xEnd, yEnd, echiquier);
+                            }
+                            else {
+                                return false;
+                            }
+                        }
+                    }
                 }
                 //Diagonale HAUT Gauche
-                else if (xStart > xEnd && yStart > yEnd)
-                {
-                    while (xStart > xEnd && yStart > yEnd)
-                    {
-                        if (echiquier[xStart, yStart] == null)
-                        {
-                            xStart--;
-                            yStart--;
-                            continue;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    }
+                else if (xStart > xEnd && yStart > yEnd) {
+                    while (xStart > xEnd && yStart > yEnd) {
+                        if (echiquier[xStart, yStart] == null) {
+                            xStart--;
+                            yStart--;
+                            continue;
+                        }
+                        else {
+                            if (echiquier[xEnd, yEnd] != null) {
+                                IsArriveeOnEnnemy(xEnd, yEnd, echiquier);
+                            }
+                            else {
+                                return false;
+                            }
+                        }
+                    }
                 }
                 //DIAGONALE BAS GAUCHE 
-                else if (xStart > xEnd && yStart < yEnd)
-                {
-                    while (xStart > xEnd && yStart < yEnd)
-                    {
-                        if (echiquier[xStart, yStart] == null)
-                        {
-                            xStart--;
-                            yStart++;
-                            continue;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    }
+                else if (xStart > xEnd && yStart < yEnd) {
+                    while (xStart > xEnd && yStart < yEnd) {
+                        if (echiquier[xStart, yStart] == null) {
+                            xStart--;
+                            yStart++;
+                            continue;
+                        }
+                        else {
+                            if (echiquier[xEnd, yEnd] != null) {
+                                IsArriveeOnEnnemy(xEnd, yEnd, echiquier);
+                            }
+                            else {
+                                return false;
+                            }
+                        }
+                    }
                 }
                 //DIAGONALE HAUT DROIT
-                else if (xStart < xEnd && yStart > yEnd)
-                {
-                    while (xStart < xEnd && yStart > yEnd)
-                    {
-                        if (echiquier[xStart, yStart] == null)
-                        {
-                            xStart++;
-                            yStart--;
-                            continue;
-                        }
-                        else
-                        {
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
+                else if (xStart < xEnd && yStart > yEnd) {
+                    while (xStart < xEnd && yStart > yEnd) {
+                        if (echiquier[xStart, yStart] == null) {
+                            xStart++;
+                            yStart--;
+                            continue;
+                        }
+                        else {
+                            if (echiquier[xEnd, yEnd] != null) {
+                                IsArriveeOnEnnemy(xEnd, yEnd, echiquier);
+                            }
+                            else {
+                                return false;
+                            }
+                        }
+                    }
+                }
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
     }
 }
